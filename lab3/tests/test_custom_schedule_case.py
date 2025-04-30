977d9c679775bd0246dc3a30f1c941d27387180a
lenow55 <99322377+lenow55@users.noreply.github.com>
2025-04-30 12:18:56 +0300

    Fix/node capacity (#4)
    * fix repair record pytest
    
    * try test node capacity
    
    При использовании schedule arrival node должен подругому
    считать количество доступных серверов
    
    * fix node capacity compute

--- lab3/tests/test_custom_schedule_case.py
+++ lab3/tests/test_custom_schedule_case.py
@@ -52,7 +52,12 @@ N = ciw.create_network(
             preemption="reroute",
             id=1,
         ),
-        1,
+        RepairedFailureSchedule(
+            failure_dist=dists.Sequential([100, 150]),
+            repair_mgr=manager,
+            preemption="reroute",
+            id=2,
+        ),
     ],
     # У дуплексного канала с двух сторон по два слота для ожидания
     # у спутниковой линии очереди нет
